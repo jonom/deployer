142a8b39bf035bfe7a8c487e08736b39429f4067
Jonathon Menz <jonomenz@gmail.com>
2020-01-28 10:16:12 -0800

    Fix Silverstripe recipe
    * Additionally check shared directory for assets since this directory is commonly excluded from project git repos
    * Ensure shared_assets returns a non-null value so the release folder can’t get accidentally deleted

--- recipe/silverstripe.php
+++ recipe/silverstripe.php
@@ -2,6 +2,8 @@
 
 namespace Deployer;
 
+use Deployer\Exception\Exception;
+
 require_once __DIR__ . '/common.php';
 
 /**
@@ -9,15 +11,10 @@ require_once __DIR__ . '/common.php';
  */
 
 set('shared_assets', function () {
-    $paths = [
-        'assets',
-        'public/assets'
-    ];
-    foreach ($paths as $path) {
-        if (test('[ -d {{release_path}}/'.$path.' ]')) {
-            return $path;
-        }
+    if (test('[ -d {{release_path}}/public ]') || test('[ -d {{deploy_path}}/shared/public ]')) {
+        return 'public/assets';
     }
+    return 'assets';
 });
 
 
